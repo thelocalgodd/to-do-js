2b921f15bf14b7a8e2ca913378d02409431c6296
thelocalgodd <vkkpemlie@st.knust.edu.gh>
2023-10-20 00:26:43 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText) {
+            document.getElementById('placeholder').remove()
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <span>${taskText}</span>
+                <button class="delete">delete</button>
+            `;
+
+            taskList.appendChild(listItem);
+            taskInput.value = "";
+
+            // Attach a click event to the delete button
+            const deleteButton = listItem.querySelector(".delete");
+            deleteButton.addEventListener("click", function () {
+                listItem.remove();
+            });
+        }
+    });
+
+    // Handle Enter key press
+    taskInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+## to-do app
+
+A simple to-do app made with HTML, CSS & JavaScript.
